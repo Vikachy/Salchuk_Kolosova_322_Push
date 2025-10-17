6b6800c32adad415141ab55bfeaea6dd0440c9e2
227095 <227095@KB37-118-C4>
2025-10-17 10:51:05 +0300

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Salchuk_Kolosova_322_Push
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
